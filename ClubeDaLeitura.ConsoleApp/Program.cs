3e4919ef7eb82fed67a78f0e2c971864ecf512d2
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-15 15:00:35 -0300

    Add project files.

---
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClubeDaLeitura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
