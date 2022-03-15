1a05b323ab6fb1462f76fd07215ce57cc193599c
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-15 20:39:12 -0300

    criacao das classes revistas e caixas e seus atributos

---
+++ ClubeDaLeitura.ConsoleApp/Caixa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp
+{
+    internal class Caixa
+    {
+        public string cor;
+        public string etiqueta;
+        public int numero;
+    }
+
+}
---
+++ ClubeDaLeitura.ConsoleApp/Revista.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp
+{
+    internal class Revista
+    {
+        public string tipo_colecao;
+        public int numero_edicao;
+        public DateTime ano_revista;
+        public Caixa[] caixas;
+
+        public int DefinirPosicaoParaCaixa()
+        {
+            int posicao = -1;//caso não exista posicao retorna -1
+            for (int i = 0; i < caixas.Length; i++)
+            {
+                if (caixas[i] == null)
+                {
+                    posicao = i;
+                    break;
+                }
+            }
+            return posicao;
+        }
+    }
+}
