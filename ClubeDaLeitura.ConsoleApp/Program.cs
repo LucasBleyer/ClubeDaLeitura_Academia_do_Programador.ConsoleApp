68b18a6e7239647192c4238cd57fd8e1f8c89d3c
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-15 21:14:37 -0300

    arrays globais para armazenar os atributos de cada classe

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -4,9 +4,14 @@ namespace ClubeDaLeitura.ConsoleApp
 {
     internal class Program
     {
+        public static Amigo[] amigos = new Amigo[100];
+        public static Caixa[] caixas = new Caixa[100];
+        public static Revista[] revistas = new Revista[100];
+        public static Emprestimo[] emprestimos = new Emprestimo[100];
+
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
